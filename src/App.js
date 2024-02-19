46578bdd5b77960c23723cafcd07786429858e60
shifa <shifab@lanatusystems.com>
2024-02-19 12:28:58 +0530

    change epath

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home, NotFound } from './Components/default';
-import { Box } from '@mui/material'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Box } from "@mui/material";
 
 //components
-import Header from './Components/Header/Header';
-import DetailView from './Components/ItemDetails/DetailView';
-import TemplateProvider from './templates/TemplateProvider';
-import ContextProvider from './context/ContextProvider';
-import Cart from './Components/Cart/Cart';
+import Header from "./Components/Header/Header";
+import DetailView from "./Components/ItemDetails/DetailView";
+import TemplateProvider from "./templates/TemplateProvider";
+import ContextProvider from "./context/ContextProvider";
+import Cart from "./Components/Cart/Cart";
+import Home from "./Components/Home";
 
 function App() {
   return (
@@ -15,11 +16,11 @@ function App() {
       <ContextProvider>
         <BrowserRouter>
           <Header />
-          <Box style={{marginTop: 54}}>
+          <Box style={{ marginTop: 54 }}>
             <Routes>
-              <Route path= '/' element={<Home />} />
-              <Route path= '/cart' element={<Cart />} />
-              <Route path= '/product/:id' element={<DetailView />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/product/:id" element={<DetailView />} />
             </Routes>
           </Box>
         </BrowserRouter>
